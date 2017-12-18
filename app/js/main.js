d2d0f3a3c69923c64f6a05881f78eda1dc47c7a6
Nurul Furqon <furqon.mascode@gmail.com>
2017-12-18 10:57:56 +0700

    [update] tikecom microsite
    microsite tiketcom

---
+++ app/js/main.js
@@ -0,0 +1,29 @@
+// scroll animation
+$(window).scroll(function () {
+    var wd = $(window).width();
+    var sc = $(window).scrollTop();
+    if (sc > 60 && wd >= 769) {
+        $('.header').addClass('header--scroll');
+        $('.header').css('padding', '12px 10%');
+    } else {
+        if (wd >= 769) {
+            $('.header').removeClass('header--scroll');
+            $('.header').css('padding', '25px 10%');
+        }
+    }
+});
+
+// slick slider
+$(document).ready(function(){
+    $('.promo__banner-content').slick({
+      dots: true,
+      centerMode: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+    });
+});
